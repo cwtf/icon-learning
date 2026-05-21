847b50eaf7e1175c8f1e507263c7d4a88981119e
cwtf <cwtf@pm.me>
2026-05-21 19:33:21 +0800

    Config: Add base path for GitHub Pages deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,7 @@ import { defineConfig } from "astro/config";
 
 export default defineConfig({
   site: "https://www.iconlearning.com.my",
+  base: "/icon-learning",
   devToolbar: {
     enabled: false,
   },
